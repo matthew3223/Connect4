31de60a37fcbf4fdec8737f4101152a5b8e47a94
Matthew <mloi4066@gmail.com>
2021-05-08 10:55:21 -0400

    Prevents users from giving weird imputs

--- connect4.java
+++ connect4.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.InputMismatchException;
 class Main {
   //function will take in a dimension of the board
   public static char[][] board (int numRow, int numCol) {
@@ -38,36 +39,174 @@ class Main {
     return(board);
   } 
   //this function checks if the move is valid or not (homework)
-  public static boolean isValidMove(board, col) {
-
+  public static boolean isValidMove(char[][] board, int col) {
+    if (col >= board[0].length || col <= -1) {
+        return(false);
+    } 
+    if (board[0][col] != '-') {
+      return(false);
+    } else {
+      return(true);
+    }
   }
   //function will take in the board and determines winning scenarios
-  /*public static boolean Winner(char[][] board) {
-          int seenAR = 0;
-          int seenAY = 0;
-    // this will go through each columns
+  public static boolean Winner(char[][] board, char player, int game) {
+    //checks the row
     for (int i = 0; i < board.length; i++) {
-         for (int j = 0; j < board.length; j++) {
-           if (board[i][j] = R) {
-             seenAR++;
-           } else if (board[i][j] = Y) {
-             seenAY++;
+         int seenColour = 0;
+         char lastChar = ' ';
+         for (int j = 0; j < board[i].length; j++) {
+           if (board[i][j] == '-') {
+             seenColour = 1;
+             lastChar = board[i][j];  
+           } else {
+             if (board[i][j] == lastChar) {
+                  seenColour++;
+                  if (seenColour == game) {
+                    return(true);
+                  }
+                  lastChar = board[i][j];
+             } else {
+                  seenColour = 1;
+                  lastChar = board[i][j];
+             }
            }
          }
-         if (seenAR == board.length || seenAY == board.length) {
-           return(true);
-         }
     }
-        //will go through the diagonals
-    for(int i = 0; i < board.length; i++) {
-      if (board[i][board])
+    //checks the column
+    for (int j = 0; j < board[0].length; j++) {
+      int seenColour = 0;
+      char lastChar = ' ';
+      for (int i = 0; i < board.length; i++) {
+        if (board[i][j] == '-') {
+          seenColour = 1;
+          lastChar = board[i][j];
+        } else {
+          if (board[i][j] == lastChar) {
+            seenColour++;
+            if (seenColour == game) {
+              return(true);
+            }
+            lastChar = board[i][j];
+          } else {
+            seenColour = 1;
+            lastChar = board[i][j];
+          }
+        }
+      }
     }
-  }*/
+        //will go through the diagonals (positive slope)
+    int i = board.length - 1;
+    int k = board[0].length - 1;
+    while (i >= 0) {
+      int j = i;  //j is handling the 1st coordinate
+      int l = k;  //i is handling the 2nd coordinate
+      int seenColour = 0;
+      char lastChar = ' ';
+      while (l < board[0].length && j >= 0) {
+          if (board[j][l] == '-') {
+             seenColour = 1;
+             lastChar = board[j][l];  
+           } else {
+             if (board[j][l] == lastChar) {
+                  seenColour++;
+                  if (seenColour == game) {
+                    return(true);
+                  }
+                  lastChar = board[j][l];
+             } else {
+                  seenColour = 1;
+                  lastChar = board[j][l];
+             }
+           }
+           j--;
+           l++;
+      }
+      k--;
+      if (k < 0) {
+        i--;
+        k = 0;
+      }
+    }
+   //will go through the diagonals (negative slope)
+    int m = board.length - 1;
+    int n  = 0;
+    while (m >= 0) {
+      int j = m;  //j is handling the 1st coordinate
+      int l = n;  //m is handling the 2nd coordinate
+      int seenColour = 0;
+      char lastChar = ' ';
+      while (j >= 0 && l >= 0) {
+          if (board[j][l] == '-') {
+             seenColour = 1;
+             lastChar = board[j][l];  
+           } else {
+             if (board[j][l] == lastChar) {
+                  seenColour++;
+                  if (seenColour == game) {
+                    return(true);
+                  }
+                  lastChar = board[j][l];
+             } else {
+                  seenColour = 1;
+                  lastChar = board[j][l];
+             }
+           }
+           j--;
+           l--;
+      }
+      n++;
+      if (n >= board[0].length - 1) {
+        n = board[0].length - 1;
+        m--;
+      }
+    }
+    return(false);
+  }
+  //this function checks if the board is full or not by knowing if anyplayer has valid moves
+  public static boolean moreValidMove(char[][] board, char player) {
+    int seenChar = 0;
+    for (int i = 0; i < board[0].length; i++) {
+      if (board[0][i] != '-') {
+        seenChar++;
+      } 
+    } 
+    if (seenChar == board[0].length) {
+        return(false);  
+    } else {
+        return(true);
+    }
+  }
   public static void main(String[] args) {
       Scanner sc = new Scanner(System.in);
+      System.out.println("Enter Your Preference: Connect_");
+      int game = 0;
+      boolean err = false;
+      do {
+        err = false;
+        try {
+          game = sc.nextInt();
+        } catch (InputMismatchException e) {
+          System.out.println("Please Enter a Number");
+          err = true;
+          sc.nextLine();
+        }
+      } while(err);
       System.out.println("Enter Board Size");
-      int numRow = sc.nextInt();
-      int numCol = sc.nextInt();
+      int numRow = 0;
+      int numCol = 0;
+      boolean er = false;
+      do {
+        er = false;
+        try {
+          numRow = sc.nextInt();
+          numCol = sc.nextInt();
+        } catch (InputMismatchException e) {
+          System.out.println("Please Enter a Number");
+          er = true;
+          sc.nextLine();
+        }
+      } while(er);
       char[][] board = board(numRow, numCol);
       display(board);
 
@@ -80,15 +219,37 @@ class Main {
           player = 'Y';
         }
         System.out.println("It's " + player + "'s turn");
-
-        int col = sc.nextInt();
+        int col = 0;
+        boolean error = false;
+        do {
+           error = false;
+           try {
+             col = sc.nextInt();
+           } catch (InputMismatchException e) {
+              System.out.println("Please Enter a Number");
+              error = true;
+              sc.nextLine(); 
+           }
+        } while (error);
+        while(!isValidMove(board, col)) {
+          System.out.println("Invalid Move");
+          col = sc.nextInt();
+        }
         board = updateBoard(board, col, player);
         display(board);
-        turn++;
+        if (Winner(board, player, game)) {
+            System.out.println(player + " wins!");
+            break;   
+        }
+        if (moreValidMove(board, player) == false) {
+            System.out.println("It's a tie");
+            break;
+        }
+        turn++;  
+        }
       }
-
   }
-}
+
     
  
 
@@ -100,39 +261,16 @@ class Main {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
